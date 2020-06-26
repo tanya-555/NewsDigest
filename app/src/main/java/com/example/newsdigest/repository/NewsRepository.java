0b081e34e22ff405639467500607a89b11cde9da
tanya-555 <52540265+tanya-555@users.noreply.github.com>
2020-06-26 22:37:14 +0530

    create repository for getting data

---
+++ app/src/main/java/com/example/newsdigest/repository/NewsRepository.java
@@ -0,0 +1,47 @@
+package com.example.newsdigest.repository;
+
+import android.os.Build;
+import android.util.Log;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+import com.example.newsdigest.models.SearchResponse;
+import com.example.newsdigest.network.ApiService;
+import com.example.newsdigest.utils.BasicUtils;
+
+import java.util.Objects;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class NewsRepository {
+
+    private static final String TAG = NewsRepository.class.getName();
+
+    private ApiService apiService;
+
+    public LiveData<SearchResponse> getNews() {
+
+        apiService = BasicUtils.connectApi().create(ApiService.class);
+
+        final MutableLiveData<SearchResponse> responseData = new MutableLiveData<>();
+        // get news list
+        Call<SearchResponse> call = apiService.getSearchResponse();
+        call.enqueue(new Callback<SearchResponse>() {
+            @Override
+            public void onResponse(Call<SearchResponse> call, Response<SearchResponse> response) {
+                responseData.setValue(response.body());
+            }
+
+            @Override
+            public void onFailure(Call<SearchResponse> call, Throwable t) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                    Log.d(TAG, Objects.requireNonNull(t.getMessage()));
+                }
+            }
+        });
+        return responseData;
+    }
+}
