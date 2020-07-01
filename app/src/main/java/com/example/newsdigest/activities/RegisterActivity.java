678d0a9b4f90d2943af1472b6f311a9ba74dc39e
tanya-555 <52540265+tanya-555@users.noreply.github.com>
2020-07-01 17:31:16 +0530

    implement registration screen validation

--- app/src/main/java/com/example/newsdigest/activities/RegisterActivity.java
+++ app/src/main/java/com/example/newsdigest/activities/RegisterActivity.java
@@ -1,7 +1,11 @@
 package com.example.newsdigest.activities;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,15 +19,24 @@ import com.example.newsdigest.di.DaggerSharedPrefComponent;
 import com.example.newsdigest.di.SharedPrefModule;
 import com.example.newsdigest.models.RegisterModel;
 import com.example.newsdigest.viewmodel.RegisterViewModel;
+import com.jakewharton.rxbinding2.view.RxView;
+
+import java.util.Objects;
 
 import javax.inject.Inject;
 
-public class RegisterActivity extends AppCompatActivity  {
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+
+public class RegisterActivity extends AppCompatActivity {
 
     private static final String TAG = RegisterActivity.class.getName();
+    private static final String USERNAME = "user_name";
+    private static final String PASSWORD = "password";
 
     private RegisterActivityBinding binding;
     private RegisterViewModel registerViewModel;
+    private CompositeDisposable disposable;
 
     @Inject
     SharedPreferences sharedPreferences;
@@ -33,6 +46,7 @@ public class RegisterActivity extends AppCompatActivity  {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.register_activity);
         setContentView(binding.getRoot());
+        disposable = new CompositeDisposable();
         DaggerSharedPrefComponent.builder().sharedPrefModule(new SharedPrefModule(this)).
                 build().inject(this);
         registerViewModel = new ViewModelProvider(this).get(RegisterViewModel.class);
@@ -50,6 +64,40 @@ public class RegisterActivity extends AppCompatActivity  {
     }
 
     private void initListener() {
+        disposable.add(RxView.clicks(binding.btnRegister).observeOn(AndroidSchedulers.mainThread())
+                .subscribe(s -> {
+                    validateInput();
+                }, e -> {
+                    Log.d(TAG, Objects.requireNonNull(e.getMessage()));
+                }));
+    }
 
+    private void validateInput() {
+        String username = binding.tvUsername.getText().toString();
+        String password = binding.tvPassword.getText().toString();
+        String confirmPassword = binding.tvConfirmPassword.getText().toString();
+        if (TextUtils.isEmpty(username) || TextUtils.isEmpty(password) || TextUtils.isEmpty(confirmPassword)) {
+            Toast.makeText(this, "All field are mandatory!", Toast.LENGTH_LONG).show();
+            return;
+        }
+        if (!password.equals(confirmPassword)) {
+            Toast.makeText(this, "Passwords do not match!", Toast.LENGTH_LONG).show();
+            return;
+        }
+        saveDetailsInPreferences(username, password);
+        launchLoginActivity();
+    }
+
+    private void saveDetailsInPreferences(String username, String password) {
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(USERNAME, username);
+        editor.putString(PASSWORD, password);
+        editor.apply();
+    }
+
+    private void launchLoginActivity() {
+        Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+        startActivity(intent);
+        this.finish();
     }
 }
