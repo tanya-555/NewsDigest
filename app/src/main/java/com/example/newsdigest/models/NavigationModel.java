dbb634565188cefdaac8a6314f8d827befaf2840
tanya-555 <52540265+tanya-555@users.noreply.github.com>
2020-07-01 19:35:30 +0530

    create navigation model and layout for navigation item

---
+++ app/src/main/java/com/example/newsdigest/models/NavigationModel.java
@@ -0,0 +1,12 @@
+package com.example.newsdigest.models;
+
+public class NavigationModel {
+
+    public int icon;
+    public String name;
+
+    public NavigationModel(int icon, String name) {
+        this.icon = icon;
+        this.name = name;
+    }
+}
