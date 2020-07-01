c585fdd252621fd7bbb8946a7b2db30799b9d7fd
tanya-555 <52540265+tanya-555@users.noreply.github.com>
2020-07-01 18:54:26 +0530

    login model

---
+++ app/src/main/java/com/example/newsdigest/models/LoginModel.java
@@ -0,0 +1,13 @@
+package com.example.newsdigest.models;
+
+public class LoginModel {
+
+    public String username;
+
+    public String password;
+
+    public LoginModel(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+}
